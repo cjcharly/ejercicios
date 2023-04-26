85111e5f669e552fc5416332d42f2515ef5a3d5f
Carlos Jimenez <cjcarlosjm@gmail.com>
2023-04-26 13:46:19 +0200

    subiendo ejercicio 3

---
+++ modulo-1/ejercicios I/ejercicio3/index.js
@@ -0,0 +1,26 @@
+/**
+*Crea dos variables, una llamada numero1 y otra llamada numero2.
+*Suma las dos variables y muestra el resultado por consola.
+*Crea otras variables de tipo number y realiza las operaciones de resta, multiplicación y división. Mostrando el resultado por consola.
+ */
+
+// Crea dos variables, una llamada numero1 y otra llamda numero2.
+const numero1 = 10;
+const numero2 = 20;
+
+//Suma las dos variables y muestra el resultado por consola.
+const suma = numero1 + numero2;
+console.log({suma});
+
+//Crea otras variables de tipo number y realiza las operaciones de resta, multiplicación y división. Mostrando el resultado por consola.
+const numero3 = 30;
+const numero4 = 40;
+
+const resta = numero3 - numero4;
+console.log({ resta });
+
+const multiplicación = numero3 * numero4;
+console.log({ multiplicación });
+
+const división = numero3 / numero4;
+console.log({ division });
