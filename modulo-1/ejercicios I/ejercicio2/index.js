9261acea757df781f4bbb922e83a4c292ae7a4fe
Carlos Jimenez <cjcarlosjm@gmail.com>
2023-04-26 13:32:38 +0200

    subiendo cambios en ejercicio 2

--- modulo-1/ejercicios I/ejercicio2/index.js
+++ modulo-1/ejercicios I/ejercicio2/index.js
@@ -8,3 +8,33 @@
  * Crea una variable llamada mejorLenguaje y asígnale el primer elemento del array lenguajesFavoritos.
  * Crea una variable llamada peorLenguaje y asígnale el último elemento del array lenguajesFavoritos.
  */
+
+
+// Crea una variable llamada nombre y asígnale tu nombre como valor.
+const nombre = "Carlos";
+
+//Crea una variable llamada edad y asígnale tu edad como valor.
+const edad = '35';
+
+//Crea una variable llamada mayorDeEdad y asígnale un valor booleano que indique si eres mayor de edad.
+const mayorDeEdad = true;
+
+//Crea una variable llamada direccion y asígnale un objeto con tu dirección.
+const direccion = {
+    calle: 'Calle Cuarteles',
+    numero: 45,
+    piso: 1,
+    puerta: AbortController,
+}
+
+//Crea una variable llamada coloresFavoritos y asígnale un array con tus colores favoritos.
+const coloresFavoritos = ['verde', 'rojo', 'azul'];
+
+//Crea una variable llamada lenguajesFavoritos y asígnale un array con tus lenguajes de programación favoritos.
+const lenguajesFavoritos =['Javascript', 'PHP'];
+
+//Crea una variable llamada mejorLenguaje y asígnale el primer elemento del array lenguajesFavoritos.
+const mejorLenguaje = lenguajesFavoritos [0];
+
+//Crea una variable llamada peorLenguaje y asígnale el último elemento del array lenguajesFavoritos.
+const peorLenguaje = lenguajesFavoritos[lenguajesFavoritos.length -1];
