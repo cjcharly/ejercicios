d084e87c0b1af6f3fbf85b90a4cdd6ac4ff287eb
Carlos Jimenez <cjcarlosjm@gmail.com>
2023-04-26 17:01:40 +0200

    subiendo ejercicio4

---
+++ modulo-1/ejercicios I/ejercicio4/index.js
@@ -0,0 +1,14 @@
+/**
+ *Crea una variable llamada saludo y asígnale un string que contenga hola.
+ *Comprueba con un if si el string saludo es igual a hola. Y saca por consola un mensaje que diga "Ha saluda
+ */
+
+
+ // Crea una variable llamada saludo y asígnale un string que contenga hola.
+const saludo = 'hola';
+
+//Comprueba con un if si el string saludo es igual a hola. Y saca por consola un mensaje que diga "Ha saluda
+
+if (saludo === 'hola'){
+    console.log('Ha saludado');
+}
